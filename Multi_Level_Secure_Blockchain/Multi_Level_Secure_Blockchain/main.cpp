3e26a76a4c899818ab68590cb7d1826acab16c97
Kendric Hood <kendrichood@athena.attlocal.net>
2019-03-08 22:39:58 -0500

    adding xcode project

---
+++ Multi_Level_Secure_Blockchain/Multi_Level_Secure_Blockchain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multi_Level_Secure_Blockchain
+//
+//  Created by Kendric Hood on 3/8/19.
+//  Copyright © 2019 Kent State University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
