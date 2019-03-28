edebbb1929b121b797d7c28f42d9ec352768aae3
Kendric Hood <khood5@kent.edu>
2019-03-28 03:52:39 -0400

    forgot to uncomment passion distribution.

--- Multi_Level_Secure_Blockchain/Multi_Level_Secure_Blockchain/Network.hpp
+++ Multi_Level_Secure_Blockchain/Multi_Level_Secure_Blockchain/Network.hpp
@@ -167,7 +167,7 @@ int Network<type_msg,peer_type>::getDelay(){
     }
     if(_distribution == POISSON){
         std::poisson_distribution<int> poissonDistribution(_avgDelay);
-        return 1;//poissonDistribution(_randomGenerator);
+        return poissonDistribution(_randomGenerator);
     }
     return -1;
 }
