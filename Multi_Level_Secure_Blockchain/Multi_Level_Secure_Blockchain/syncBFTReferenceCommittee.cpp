ccec052f5a2d5281773cade239f509f80d9819bc
Kendric Hood <khood5@kent.edu>
2019-04-16 15:43:57 -0400

    rm old files

--- Multi_Level_Secure_Blockchain/Multi_Level_Secure_Blockchain/syncBFTReferenceCommittee.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  syncBFTReferenceCommittee.cpp
-//  Multi_Level_Secure_Blockchain
-//
-//  Created by Kendric Hood on 4/8/19.
-//  Copyright © 2019 Kent State University. All rights reserved.
-//
-
-#include "syncBFTReferenceCommittee.hpp"
