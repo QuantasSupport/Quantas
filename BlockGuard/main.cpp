ad5ebf5610f49f904b8c022b46378fa04f73c5d5
Kendric Hood <khood5@kent.edu>
2019-04-26 13:18:06 -0400

    made a config change

--- BlockGuard/main.cpp
+++ BlockGuard/main.cpp
@@ -350,8 +350,8 @@ void bsg(std::ofstream &csv, std::ofstream &log,int delay){
     system.setFaultTolerance(0.3);
 
     int numberOfRequests = 0;
-    for(int i =-1; i < 1000; i++){
-        if(i%5 == 0 ){system.makeRequest();}
+    for(int i =-1; i < 200; i++){
+        if(i%2 == 0 ){system.makeRequest();}
         numberOfRequests++;
         system.receive();
         system.preformComputation();
