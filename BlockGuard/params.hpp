150fbc4a2ae4400e0d5b98f712b3406724d80a3f
Kendric Hood <khood5@kent.edu>
2019-07-18 02:10:49 -0400

    param update

--- BlockGuard/params.hpp
+++ BlockGuard/params.hpp
@@ -15,7 +15,7 @@ static const int PEER_COUNT = 1024;  // 1024
 static const int GROUP_SIZE = 32;   // Fixed only 32
 static const double FAULT = 0.6;
 static const int NUMBER_OF_BYZ =  PEER_COUNT * 0.29; // 1/3
-static const int NUMBER_OF_ROUNDS = 100; 
+static const int NUMBER_OF_ROUNDS = 1000; 
 static const int NUMBER_OF_RUNS = 5;
 
 #endif /* params_hpp */
