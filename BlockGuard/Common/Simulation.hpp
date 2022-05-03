9d5a3fc2c4a9daf449dcaecd52dc93dbc2a91329
Joe <ogliojoseph2@gmail.com>
2022-05-02 21:39:08 -0400

    Fix failed merge

--- BlockGuard/Common/Simulation.hpp
+++ BlockGuard/Common/Simulation.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 //  Simulation.hpp
 //  BlockGuard
@@ -6,13 +5,10 @@
 //  Created by Joseph Oglio on 4/14/22.
 //  Copyright © 2022 Kent State University. All rights reserved.
 //
-=======
 // This class handles reading in a configuration file, setting up log files for the simulation, 
 // initializing the network class, and repeating a simulation according to the configuration file 
 // (i.e., running multiple experiments with the same configuration).  It is templated with a user 
 // defined message and peer class, used for the underlaying network instance. 
-
->>>>>>> 2f4e9ab31a45a81bb5c0deba8c0e533eda7388d0
 
 #ifndef Simulation_hpp
 #define Simulation_hpp
@@ -22,21 +18,7 @@
 using std::ofstream;
 
 namespace blockguard {
-<<<<<<< HEAD
 	template<class type_msg, class peer_type>
-	class Simulation {
-	private:
-		Network<type_msg, peer_type> 		system;
-
-	public:
-		// Config file
-		void 				run(json config);
-
-		// logging functions
-		ostream& printTo(ostream& out)const;
-		friend ostream& operator<<  (ostream& out, const Simulation& sim) { return sim.printTo(out); };
-=======
-    template<class type_msg, class peer_type>
     class Simulation {
     private:
         Network<type_msg, peer_type> 		system;
@@ -49,11 +31,9 @@ namespace blockguard {
         // logging functions
         ostream& 			printTo		(ostream &out)const;
         friend ostream&     operator<<  (ostream &out, const Simulation &sim)      {return sim.printTo(out);};
->>>>>>> 2f4e9ab31a45a81bb5c0deba8c0e533eda7388d0
 
     };
 
-<<<<<<< HEAD
 	template<class type_msg, class peer_type>
 	ostream& Simulation<type_msg, peer_type>::printTo(ostream& out)const {
 		return out;
@@ -94,49 +74,6 @@ namespace blockguard {
 		LogWritter::instance()->print();
 		out.close();
 	}
-=======
-    template<class type_msg, class peer_type>
-    ostream& Simulation<type_msg,peer_type>::printTo(ostream &out)const{
-        return out;
-    }
-
-    template<class type_msg, class peer_type>
-    void Simulation<type_msg, peer_type>::run(json config) {
-        for (int i = 1; i <= config["tests"]; i++) {
-            ofstream out;
-            
-            if (config["logFile"] == "cout") {
-                system.setLog(cout); // Set the log file to the console
-            }
-            else {
-                string file = config["logFile"];
-                file += "Test" + std::to_string(i) + ".log";
-                out.open(file);
-                if (out.fail()) {
-                    cout << "Error: could not open file " << file << std::endl;
-                    system.setLog(cout); // If the file doesn't open set the log file to the console
-                }
-                else {
-                    system.setLog(out); // Otherwise set the log file to the user given file
-                }
-                
-            }
-            // Configure the delay properties and initial topology of the network
-            system.setDistribution(config["distribution"]);
-            system.initNetwork(config["topology"]);
-
-            for (int j = 1; j <= config["rounds"]; j++) {
-                system.receive(); // do the receive phase of the round
-                system.log(); // log the system state
-                system.performComputation();  // do the perform computation phase of the round
-                system.log();
-                system.transmit(); // do the transmit phase of the round
-                system.log();
-            }
-        }
-    }
->>>>>>> 2f4e9ab31a45a81bb5c0deba8c0e533eda7388d0
-
 }
 
 #endif /* Simulation_hpp */
